09b4842edcd9444c5679f62eed374abe989e8da0
Kate Cobb <KateEcobb@gmail.com>
2015-08-04 10:21:46 -0700

    Basic route + click handling done, going to work on integrating POPOS

--- app.js
+++ app.js
@@ -1,111 +1,115 @@
-var map;
-var oneSweetTreat;
-var directionsDisplay;
-var directionsService; 
-var stepDisplay; 
-var infowindow = new google.maps.InfoWindow();
+var app; 
 
-var placesArray = [];
+$(function(){ 
+  app = {
 
-var app = angular.module('sweetTreat', []);
+     map: null,
+     oneSweetTreat: null,
+     directionsDisplay:null,
+     directionsService: null,
+     infowindow: new google.maps.InfoWindow(),
+     HR: null, 
 
-app.controller('mapCtrl', function($scope){ 
-  $scope.map = new google.maps.LatLng(37.783910, -122.408978);
+     placesArray: [],
 
-})
+    initialize: function(){
+      //instantiate directions
+      app.directionsService = new google.maps.DirectionsService();
+      
+      //create a map centered on HR
+      app.HR = new google.maps.LatLng(37.783910, -122.408978);
+      var mapOptions = {
+        center: app.HR,
+        zoom: 16,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+      app.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+      
+      //direction renderer
+      var rendererOpts = { 
+        map: app.map 
+      }
+      app.directionsDisplay = new google.maps.DirectionsRenderer(rendererOpts); 
+      app.finder()
+      //listeners
+      $('#newST').on('click', app.finder);
 
+    }, 
 
+    finder: function(){ 
+      var sweetTreats = { 
+        location: app.HR, 
+        radius: 10000,
+        openNow: true,
+        keyword: 'dessert'
+      }
 
+      var places = new google.maps.places.PlacesService(app.map);
+      app.performSearch(sweetTreats, places, function(){ 
+        app.calcRoute();
+        app.handleInfo();
+        console.log(app.oneSweetTreat)
+      });
+    },
 
+    handleInfo: function(){ 
+      $('#desc').html('<p>'+ app.oneSweetTreat.name + '</p>' + '<p>'+ app.oneSweetTreat.vicinity + '</p>' + '<p> Rating: ' +app.oneSweetTreat.rating + '/5 </p>' )
 
-var initialize = function(){
+    },
+
+    calcRoute: function(){ 
+      for (var i=0; i<app.placesArray.length; i++){ 
+        app.placesArray[i].setMap(null);
+      }
+      //waypoints will be stored as an array of objects -- location and stopover(optional)
+
+      var start = 'Hack Reactor, 944 market st, San Francisco, California'; 
+      var end = app.oneSweetTreat.geometry.location;
+
+      var request = { 
+        origin: start, 
+        destination: end, 
+        travelMode: google.maps.TravelMode.WALKING
+      }
+
+      app.directionsService.route(request, function(response, status){ 
+        if(status == google.maps.DirectionsStatus.OK){ 
+          app.directionsDisplay.setDirections(response); 
+        }
+      });
+    },
+
+    performSearch: function(y, x, callback){ 
+      x.nearbySearch(y, function(results, status){ 
+        if(status !== google.maps.places.PlacesServiceStatus.OK){ 
+          alert(status);
+          return;
+        }
+        app.oneSweetTreat = results[Math.floor(Math.random()*results.length)]
+        callback(app.oneSweetTreat)
+      });
+    },
+
+    createMarker: function(place){ 
+      var placeLoc = place.geometry.location; 
+      var marker = new google.maps.Marker({ 
+        map: map, 
+        position: place.geometry.location
+      })
+
+      google.maps.event.addListener(marker, 'click', function(){ 
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+      });
+    },
   
-  //instantiate directions
-  directionsService = new google.maps.DirectionsService();
-  
-  //create a map centered on HR
-  var HR = new google.maps.LatLng(37.783910, -122.408978);
-  var mapOptions = {
-    center: HR,
-    zoom: 16,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
-  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-  
-  //direction renderer
-  var rendererOpts = { 
-    map: map 
-  }
-  
-  directionsDisplay = new google.maps.DirectionsRenderer(rendererOpts); 
-  //info window for specific directions TODO
-
-  var sweetTreats = { 
-    location: HR, 
-    radius: 60000,
-    openNow: true,
-    keyword: 'dessert'
-  }
-  stepDisplay = new google.maps.InfoWindow();
-
-var places = new google.maps.places.PlacesService(map);
-performSearch(sweetTreats, places, function(){ 
-  console.log(oneSweetTreat)
-  calcRoute();
-  this.trigger()
-});
-
-}; //end of initialize
-
-var calcRoute = function(){ 
-  for (var i=0; i<placesArray.length; i++){ 
-    placesArray[i].setMap(null);
-  }
-  //waypoints will be stored as an array of objects -- location and stopover(optional)
-
-  var start = 'Hack Reactor, San Francisco, California'; 
-  var end = oneSweetTreat.geometry.location;
-
-  var request = { 
-    origin: start, 
-    destination: end, 
-    travelMode: google.maps.TravelMode.WALKING
-  }
-
-  directionsService.route(request, function(response, status){ 
-    if(status == google.maps.DirectionsStatus.OK){ 
-      directionsDisplay.setDirections(response); 
-    }
-  });
-};
-
-var performSearch = function(y, x, callback){ 
-  x.nearbySearch(y, function(results, status){ 
-    if(status !== google.maps.places.PlacesServiceStatus.OK){ 
-      alert(status);
-      return;
-    }
-    oneSweetTreat = results[Math.floor(Math.random()*results.length)]
-    callback(oneSweetTreat)
-
-    // createMarker(oneSweetTreat)
-  });
-};
-
-var createMarker = function(place){ 
-  var placeLoc = place.geometry.location; 
-  var marker = new google.maps.Marker({ 
-    map: map, 
-    position: place.geometry.location
-  })
-
-  google.maps.event.addListener(marker, 'click', function(){ 
-    infowindow.setContent(place.name);
-    infowindow.open(map, this);
-  });
-};
-
-google.maps.event.addListener()
+  };
+}());
 
 
-google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+
+
+
