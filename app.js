79b94fc8e2346382f088a13be444c3cfa669d2e3
Kate Cobb <KateEcobb@gmail.com>
2015-08-04 18:24:15 -0700

    Finished MVP

--- app.js
+++ app.js
@@ -13,6 +13,29 @@ $(function(){
 
      placesArray: [],
 
+     icons: { 
+      cone: { 
+        url: 'ST.png', 
+        size: new google.maps.Size(44,32), 
+        origin: new google.maps.Point(0,0), 
+        anchor: new google.maps.Point(22,32)
+      },
+      park: {
+        url: 'tree.png', 
+        size: new google.maps.Size(44,32), 
+        origin: new google.maps.Point(0,0), 
+        anchor: new google.maps.Point(22,32)
+      }, 
+      hackReactor: {
+        url: 'logo.png', 
+        size: new google.maps.Size(44,32), 
+        origin: new google.maps.Point(0,0), 
+        anchor: new google.maps.Point(22,32)
+      }
+     },
+
+     styles: [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}],
+
     initialize: function(){
       //instantiate directions
       app.directionsService = new google.maps.DirectionsService();
@@ -22,18 +45,26 @@ $(function(){
       var mapOptions = {
         center: app.HR,
         zoom: 16,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        styles: app.styles
       }
       app.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
       
       //direction renderer
       var rendererOpts = { 
-        map: app.map 
+        map: app.map
       }
+
       app.directionsDisplay = new google.maps.DirectionsRenderer(rendererOpts); 
       app.finder()
       //listeners
       $('#newST').on('click', app.finder);
+      setInterval(function() {
+        var red   = Math.floor(Math.random() * 256);
+        var green = Math.floor(Math.random() * 256);
+        var blue  = Math.floor(Math.random() * 256);
+        $('#newST').css('background-color', 'rgb(' + red + ',' + green + ',' + blue + ')');
+      },1000);
 
     }, 
 
@@ -54,8 +85,6 @@ $(function(){
            }
 
         app.performParkSearch(parks, app.places, function(){ 
-          console.log(app.oneSweetTreat)
-          console.log(app.onePOPO)
           app.calcRoute();
           app.handleInfo();
         });
@@ -86,6 +115,10 @@ $(function(){
       app.directionsService.route(request, function(response, status){ 
         if(status == google.maps.DirectionsStatus.OK){ 
           app.directionsDisplay.setDirections(response); 
+          var leg = response.routes[0].legs;
+          app.createMarker(leg[0].start_location, app.icons.hackReactor);
+          app.createMarker(leg[0].end_location, app.icons.cone);
+          app.createMarker(leg[1].end_location, app.icons.park);
         }
       });
     },
@@ -112,18 +145,13 @@ $(function(){
       });
     },
 
-    createMarker: function(place){ 
-      var placeLoc = place.geometry.location; 
+    createMarker: function(place, icon){ 
       var marker = new google.maps.Marker({ 
-        map: map, 
-        position: place.geometry.location
+        map: app.map, 
+        position: place, 
+        icon: icon
       })
-
-      google.maps.event.addListener(marker, 'click', function(){ 
-        infowindow.setContent(place.name);
-        infowindow.open(map, this);
-      });
-    },
+    }
   
   };
 }());
